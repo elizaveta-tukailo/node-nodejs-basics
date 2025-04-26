e488d0b8c21fb1fdf0a90b28cc3b0c6cb58346c9
elizaveta-tukailo <lizatukailo@gmail.com>
2025-04-27 00:23:51 +0300

    feat: add functions to work with the cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const formattedArgs = args.reduce((acc, arg, index, array) => {
+        if (arg.startsWith("--")) {
+          acc[arg.slice(2)] = array[index + 1] || ""; 
+        }
+        return acc;
+    }, {});
+
+    const outputArgs = Object.entries(formattedArgs)
+        .map(([argName, value]) => `${argName} is ${value}`)
+        .join(', ');
+
+    console.log(outputArgs);  
 };
 
 parseArgs();
