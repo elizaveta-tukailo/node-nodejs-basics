ca97f472bb8a638cca4d3d0c100863b001506bb5
elizaveta-tukailo <lizatukailo@gmail.com>
2025-04-27 23:10:00 +0300

    feat: add functions to work with the streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
+    const fileStream = createReadStream(filePath, { encoding: 'utf8' });
+    try {
+        await pipeline(
+            fileStream,
+            process.stdout
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await read();
