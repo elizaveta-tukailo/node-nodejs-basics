82aae16a6dac31fa102d1506c849f9dfaa1d29f8
elizaveta-tukailo <lizatukailo@gmail.com>
2025-04-27 22:10:45 +0300

    feat: add function to calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const fileName = 'fileToCalculateHashFor.txt';
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, "files", fileName);
+
+    try {
+        const stream = createReadStream(filePath);
+        await pipeline(stream, hash);
+        console.log(hash.digest('hex'));
+    } catch (error) {
+        console.log('error', error);
+    }
 };
 
 await calculateHash();
