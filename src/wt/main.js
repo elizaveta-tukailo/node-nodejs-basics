4282cdf3ddd46f7860b3725f1dfdecc2b0906b57
elizaveta-tukailo <lizatukailo@gmail.com>
2025-04-28 20:10:18 +0300

    feat: implement functions that use worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'node:os';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const cpuCoreCount = cpus().length;
+
+    const workers = [];
+    for (let i = 0; i < cpuCoreCount; i++) {
+        workers.push(new Promise(resolve => {
+            const worker = new Worker(path.join(__dirname, 'worker.js'));
+    
+            worker.postMessage(10 + i);
+    
+            worker.on('message', data => {
+                resolve({ status: 'resolved', data });
+                worker.terminate();
+            });
+            worker.on('error', () => resolve({ status: 'error', data: null }));
+            worker.on('exit', code => code !== 0 && resolve({ status: 'error', data: null }));
+        }));
+    }
+    
+    const results = await Promise.all(workers);
+    console.log(results);
 };
 
-await performCalculations();
+performCalculations();
